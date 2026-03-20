3ca1177948730968ff9fe30624ce6098be6d770c
yusuferdemkavak <yusuferdemkavak@hotmail.com>
2026-03-20 15:11:02 +0300

    sphysics functions added but not implemented

--- sphysics.c
+++ sphysics.c
@@ -1,2 +1,79 @@
 #include "sphysics.h"
 
+
+// Collision Detection (Identical Objects) //
+bool CheckCollisionPoints(Point point_1, Point point_2)
+{
+
+}
+
+bool CheckCollisionLines(Line line_1, Line line_2)
+{
+
+}
+
+bool CheckCollisionTriangles(Triangle triangle_1, Triangle triangle_2)
+{
+
+}
+
+bool CheckCollisionRectangles(Rectangle rectangle_1, Rectangle rectangle_2)
+{
+
+}
+
+bool CheckCollisionCircles(Circle circle_1, Circle circle_2)
+{
+
+}
+
+// Collision Detection (Distinct Objects) //
+bool CheckCollisionPointToLine(Point point, Line line)
+{
+
+}
+
+bool CheckCollisionPointToTriangle(Point point, Triangle triangle)
+{
+
+}
+
+bool CheckCollisionPointToRectangle(Point point, Rectangle rectangle)
+{
+
+}
+
+bool CheckCollisionPointToCircle(Point point, Circle circle)
+{
+
+}
+
+bool CheckCollisionLineToTriangle(Line line, Triangle triangle)
+{
+
+}
+
+bool CheckCollisionLineToRectangle(Line line, Rectangle rectangle)
+{
+
+}
+
+bool CheckCollisionLineToCircle(Line line, Circle circle)
+{
+
+}
+
+bool CheckCollisionTriangleToRectangle(Triangle triangle, Rectangle rectangle)
+{
+
+}
+
+bool CheckCollisionTriangleToCircle(Triangle triangle, Circle circle)
+{
+
+}
+
+bool CheckCollisionRectangleToCircle(Rectangle rectangle, Circle circle)
+{
+
+}
