11b2ece570cd8ac155a8ab12754f0e51c1342de3
yusuferdemkavak <yusuferdemkavak@hotmail.com>
2026-03-21 21:02:13 +0300

    window function added

--- sdraw.h
+++ sdraw.h
@@ -4,6 +4,13 @@
 #include "swipe.h"
 #include "sphysics.h"
 
+//-----Window Properties-----/
+typedef struct Resolution
+{
+	int width;
+	int height;
+} Resolution;
+
 //-----Drawing Porperties-----//
 typedef struct Color
 {
@@ -17,7 +24,11 @@ typedef enum Shape_Type
 // Whether the shape is filled inside
 {
 	EMPTY,
-	FILLED
+	FILLED,
+	STRIPEDV,
+	STRIPED_VERTICAL = STRIPEDV, // STRIPEDV Alias
+	STRIPEDH,
+	STRIPED_HORIZONTAL = STRIPEDH // STRIPEDH Alias
 } Shape_Type;
 
 //-----Geometric Objects-----//
@@ -68,18 +79,22 @@ typedef struct DCircle
 typedef DCircle DCirc; // DCircle Alias
 
 //-----Function Declaration-----//
-// Drawing Functions //
-void DrawPoint(DPoint point);
-void DrawLine(Dline line);
-void DrawTriangle(DTriangle triangle);
-void DrawRectangle(DRectangle rectangle);
-void DrawCircle(DCircle circle);
+//-----Drawing Functions-----//
+// Window //
+DrawWindow(Resolution res);
 
-// Drawable Converters //
-DPoint DrawablePoint(Point point, Color color, Shape_Type type);
-DLine DrawableLine(Line line, Color color, Shape_Type type);
-DTriangle DrawableTriangle(Triangle triangle, Color color, Shape_Type type);
-DRectangle DrawableRectangle(Rectangle rectangle, Color color, Shape_Type type);
-DCircle DrawableCircle(Circle circle, Color color, Shape_Type type);
+// Objects //
+void DrawPoint(DPoint point); // Draws a Point on the window
+void DrawLine(Dline line); // Draws a Line on the window
+void DrawTriangle(DTriangle triangle); // Draws a Triangle on the window
+void DrawRectangle(DRectangle rectangle); // Draws a Rectangle on the window
+void DrawCircle(DCircle circle); // Draws a Circle on the window
+
+//-----Drawable Converters-----//
+DPoint DrawablePoint(Point point, Color color, Shape_Type type); // Returns  DPoint from a Point and drawing properties
+DLine DrawableLine(Line line, Color color, Shape_Type type); // Returns a DLine from a Line and drawing properties
+DTriangle DrawableTriangle(Triangle triangle, Color color, Shape_Type type); // Returns a DTriangle from a Triangle and drawing properties
+DRectangle DrawableRectangle(Rectangle rectangle, Color color, Shape_Type type); // Returns a DRectangle from a Rectangle and drawing properties
+DCircle DrawableCircle(Circle circle, Color color, Shape_Type type); // Returns a DCircle from a circle and drawing properties
 
 #endif
